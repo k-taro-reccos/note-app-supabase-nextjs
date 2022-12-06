71888adc5989ce6a751288b72496e462b6d018d6
k-taro-reccos <tarou3806@gmail.com>
2022-12-06 20:07:38 +0900

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,44 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { useRouter } from 'next/router'
+import { supabase } from 'utils/supabase'
+import { useEffect } from 'react'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { push, pathname } = useRouter()
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if (user && pathname === '/') {
+      push('notes')
+    } else if (!user && pathname !== '/') {
+      await push('/')
+    }
+  }
+  supabase.auth.onAuthStateChange((event) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/notes')
+    }
+    if (event === 'SIGNED_OUT') {
+      push('/')
+    }
+  })
+  useEffect(() => {
+    validateSession()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
